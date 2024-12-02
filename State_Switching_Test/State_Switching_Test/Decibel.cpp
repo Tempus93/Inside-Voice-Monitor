c512e7371505466a60dc5e9b17d408554ac603ec
Justin Downer <justindowner121@gmail.com>
2024-12-01 23:05:24 -0500

    Add files via upload

---
+++ State_Switching_Test/State_Switching_Test/Decibel.cpp
@@ -0,0 +1,112 @@
+#include "Decibel.h"
+
+const float NOISE_FLOOR = 20.0;   // Minimum threshold to filter out background noise
+const int SAMPLE_WINDOW = 50;     // Number of samples to collect & pin
+int RLED, GLED, BLED, SensorPin;
+
+
+DecibelMeter::DecibelMeter(float calibrationFactor) 
+    : _pin(SensorPin), _calibrationFactor(calibrationFactor) {}
+
+float DecibelMeter::getVoltage(int rawValue) {
+    return rawValue * (5.0 / 1023.0);
+}
+
+//pins setup
+void DecibelMeter::setup(int microphone_pin, int r_led, int g_led, int b_led){
+  SensorPin = microphone_pin;
+  RLED = r_led; 
+  GLED = g_led;
+  BLED = b_led;
+
+  pinMode(SensorPin, INPUT);
+  pinMode(GLED, OUTPUT);
+  pinMode(RLED, OUTPUT);
+  pinMode(GLED, OUTPUT);
+  
+}
+
+
+float DecibelMeter::calculateSPL(float voltage) {
+    // Conversion formula 
+    float decibels = 20 * log10(voltage) + 70;
+    return max(decibels, NOISE_FLOOR);
+}
+
+int DecibelMeter::measureDecibels() {
+    unsigned long startMillis = millis();
+    float peakToPeak = 0;
+    unsigned int signalMax = 0;
+    unsigned int signalMin = 1023;
+
+    // Collect samples over a short window
+    while (millis() - startMillis < SAMPLE_WINDOW) {
+        int sample = analogRead(_pin);
+        
+        // Track peak-to-peak voltage
+        if (sample < 1024) {
+            if (sample > signalMax) {
+                signalMax = sample;
+            }
+            else if (sample < signalMin) {
+                signalMin = sample;
+            }
+        }
+    }
+
+    // Calculate peak-to-peak voltage
+    peakToPeak = (signalMax - signalMin) * (5.0 / 1023.0);
+    
+    // Convert to decibels with calibration and round to nearest integer
+    int decibels = round(calculateSPL(peakToPeak) * _calibrationFactor);
+    
+    return decibels;
+}
+
+void DecibelMeter::loudness_LED(int db){
+   
+    if(db <= 52){
+      //Green led blinks(low volume, bring it up)
+      digitalWrite(GLED, 0);
+      delay(300);
+      digitalWrite(GLED, 1);
+      delay(300);
+      digitalWrite(GLED, 0);   
+    }
+
+    if(db > 52 && db <= 57){
+      // perfect volume, NO NOTES :D
+      digitalWrite(RLED, 0);
+      digitalWrite(GLED,1);
+    }
+
+    if(db > 57 && db <= 63){
+      // perfect volume, NO NOTES :D
+      digitalWrite(GLED,0);
+      digitalWrite(RLED,1);
+    }
+
+    if(db > 63){
+      // NOOO TOOO LOUDD
+      digitalWrite(GLED, 0);
+      digitalWrite(RLED,0);
+      delay(150);
+      digitalWrite(RLED,1);
+      delay(150);
+      digitalWrite(RLED,0);
+
+    }
+
+}
+
+char DecibelMeter::loudness_Char(){
+  int db = measureDecibels();
+  loudness_LED(db);
+  if(db <= 52){return "Quiet";}
+
+  if(db > 52 && db <= 57){ return "Perfect";}
+
+  if(db > 58 && db <= 63){return "getting loud";}
+
+  if(db > 63){return "TOO LOUD";}
+}
---
+++ State_Switching_Test/State_Switching_Test/Distance.cpp
@@ -0,0 +1,89 @@
+#include "Distance.h"
+//Two things I want the distance file to be done with
+ int EchoPin;
+ int TrigPin;
+
+//Setups the UltraSonic Sensor's Pins
+Distance::Distance(int in)
+  : thro(in),
+    inrangestart(0){}
+
+//setup pins
+void Distance::setupUSSensor(int Echo, int Trig){
+  EchoPin = Echo; 
+  TrigPin = Trig;
+
+  pinMode(Echo, INPUT);
+  pinMode(Trig, OUTPUT);
+
+}
+
+//Allows the sensor to send out a pulse in a 2 ms interval.
+void Distance::Pulse(){
+  digitalWrite(TrigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TrigPin, HIGH);
+  delayMicroseconds(2);
+  digitalWrite(TrigPin, LOW);
+}
+
+// Reads the time of receiving the pulse then converts it into CMs.
+long Distance::PulsetoCentimeters(){
+  long distance = pulseIn(EchoPin, HIGH, 1000000);
+  return distance / 29 / 2;
+}
+
+bool Distance::inrange(int duration){
+  long d = PulsetoCentimeters();
+
+  // when person is in this distance range
+  if(d <= 40){
+
+     if (inrangestart == 0) {
+
+          //if its still 0 while in range, get current millis since machine start
+          inrangestart = millis();
+        }
+        
+        // Check if we've been in range long enough (3s) )
+        if (millis() - inrangestart >= duration) {
+            // Reset timer for next check
+            inrangestart = 0;
+            return true;
+        }
+     else {
+        // Reset timer if out of range
+        inrangestart = 0;
+    }
+
+    return false;
+  }
+}
+
+bool Distance::Ninrange(int duration){
+
+    long d = PulsetoCentimeters();
+
+  // when person is in this distance range
+  if(d > 40){
+
+     if (Ninrangestart == 0) {
+
+          //if its still 0 while in range, get current millis since machine start
+          Ninrangestart = millis();
+        }
+        
+        // Check if we've been in range long enough (3s) )
+        if (millis() - Ninrangestart >= duration) {
+            // Reset timer for next check
+            Ninrangestart = 0;
+            return true;
+        }
+     else {
+        // Reset timer if out of range
+        Ninrangestart = 0;
+    }
+
+    return false;
+  }  
+}
